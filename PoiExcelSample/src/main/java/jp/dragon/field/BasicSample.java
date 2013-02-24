dab584523cb31fd3a514bda1cc55f39ec51e2784
dragonfield <tatsuta@js2.so-net.ne.jp>
2013-02-24 23:04:49 +0900

    indent fix

--- PoiExcelSample/src/main/java/jp/dragon/field/BasicSample.java
+++ PoiExcelSample/src/main/java/jp/dragon/field/BasicSample.java
@@ -13,30 +13,30 @@ public class BasicSample {
 	public static void main(String[] args) throws Exception {
 		Workbook workbook = new HSSFWorkbook();
 		Sheet sheet = workbook.createSheet("Sheet1");
-
+		
 		Row row = sheet.createRow(0);
 		Cell cell = row.createCell(0);
-        cell.setCellValue("datetime");
+		cell.setCellValue("datetime");
 		cell = row.createCell(1);
-        cell.setCellValue("session");
-        
-        row = sheet.createRow(1);
-        cell = row.createCell(0);
-        cell.setCellValue("2012/02/03 05:00:00");
-        cell = row.createCell(1);
-        cell.setCellValue("5");
-
-        row = sheet.createRow(2);
-        cell = row.createCell(0);
-        cell.setCellValue("2012/02/03 05:00:00");
-        cell = row.createCell(1);
-        cell.setCellValue("7");
-        
+		cell.setCellValue("session");
+		
+		row = sheet.createRow(1);
+		cell = row.createCell(0);
+		cell.setCellValue("2012/02/03 05:00:00");
+		cell = row.createCell(1);
+		cell.setCellValue("5");
+		
+		row = sheet.createRow(2);
+		cell = row.createCell(0);
+		cell.setCellValue("2012/02/03 05:00:00");
+		cell = row.createCell(1);
+		cell.setCellValue("7");
+		
 		FileOutputStream fileOut = new FileOutputStream("result.xls");
-        workbook.write(fileOut);
-        fileOut.close();
-        
-        System.out.println("finished");
+		workbook.write(fileOut);
+		fileOut.close();
+		
+		System.out.println("finished");
 	}
 
 }
